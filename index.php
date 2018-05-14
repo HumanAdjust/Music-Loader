d0eb02db44cceeac905b981c4dafdcd30bcd9d4c
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2018-05-14 13:13:48 +0900

    Add files via upload
    This is Main Page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Music-Loader Main Page</title>
+</head>
+
+<body>
+  <?php
+  echo "<audio src='Music\End Credits Part 1.mp3' autoplay controls>
+          브라우저가 오디오를 지원하지 않습니다.
+        </audio>";
+   ?>
+</body>
+</html>
