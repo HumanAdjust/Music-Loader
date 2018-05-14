0b34d9fe72cf9d6269cdd570eb9f55f4963deff4
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2018-05-14 13:21:10 +0900

    It's a main page for this website.
    Beta ver.0.2
    -Main content-
     ●Music Interlink Experiment
     ●Change Warning Language

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
   <?php
   echo "<audio src='Music\End Credits Part 1.mp3' autoplay controls>
-          브라우저가 오디오를 지원하지 않습니다.
+          Your browser does not support audio files.
         </audio>";
    ?>
 </body>
